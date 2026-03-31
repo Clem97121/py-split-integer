53d1d5289410b387d6c06b9fd16e9b6ab06779f0
Anton Gaievyi <anton.gaievyi@gmail.com>
2026-03-31 13:58:27 +0200

    remade tests for py-split-integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -3,6 +3,7 @@ from app.split_integer import split_integer
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(split_integer(17, 4)) == 17
+    assert sum(split_integer(11, 3)) == 11
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -14,8 +15,9 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
+    assert split_integer(11, 3) == [3, 4, 4]
     assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
-def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
+def test_should_return_parts_with_max_difference_of_one() -> None:
     assert split_integer(5, 4) == [1, 1, 1, 2]
